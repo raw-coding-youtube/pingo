740c5948864e4bb983d26400efe71603d85e9d49
T0shik <antonwieslander@hotmail.com>
2021-05-12 21:50:47 +0100

    auth handler

--- src/Pingo/Startup.cs
+++ src/Pingo/Startup.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Pingo.CustomAuthR;
 using Pingo.Hubs;
 
@@ -17,6 +23,7 @@ namespace Pingo
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -24,7 +31,8 @@ namespace Pingo
             services.AddSingleton<RoomManager>();
             services.AddSingleton<IUserIdProvider, UserIdProvider>();
             services.AddAuthentication("my_scheme")
-                .AddCookie("my_scheme", options =>
+                .AddScheme<CookieAuthenticationOptions, myAuthenticationHandler>("my_scheme", null)
+                .AddCookie("my_cookie", options =>
                 {
                     options.Cookie.HttpOnly = false;
                     options.Cookie.Name = "AuthCookie";
@@ -62,6 +70,37 @@ namespace Pingo
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapHub<ChatHub>("/ChatHub");
             });
+        }
+    }
+
+    public class myAuthenticationHandler : CookieAuthenticationHandler
+    {
+        public myAuthenticationHandler(
+            IOptionsMonitor<CookieAuthenticationOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock
+            ) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var result = await base.HandleAuthenticateAsync();
+            if (result.Succeeded)
+            {
+                return result;
+            }
+
+            var guid = Guid.NewGuid().ToString();
+            var claim = new Claim("UserId", guid);
+
+            ClaimsIdentity identity = new ClaimsIdentity(new[] { claim }, "my_scheme");
+            ClaimsPrincipal claimsPrincipal=new ClaimsPrincipal(identity);
+            await Context.SignInAsync("my_scheme", claimsPrincipal);
+            // await HttpContext.SignInAsync("my_scheme", new ClaimsPrincipal(identity));
+            AuthenticationTicket ticket=new AuthenticationTicket(claimsPrincipal,"my_scheme");
+            return AuthenticateResult.Success(ticket);
         }
     }
 
